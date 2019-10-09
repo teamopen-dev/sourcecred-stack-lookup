b067547972a0423295cc653fe7d8c77354ba8bf1
Robin <robin@robin-it.com>
2019-10-09 19:54:39 +0200

    Don't include axios in our browser bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import json from "rollup-plugin-json";
 
 export default {
   input: "src/client.js",
+  external: ['axios'],
   output: {
     format: "umd",
     name: "TeamOpenSourcecredStackLookup",
