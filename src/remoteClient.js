ae6e5b589c0d314fbb72e2f79945a8f7b1e97cc6
Robin <robin@robin-it.com>
2019-10-10 17:46:39 +0200

    Experiment with "nice" gzip.

--- src/remoteClient.js
+++ src/remoteClient.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const {gzip, ungzip} = require('pako/dist/pako.js');
+const delay = require('delay');
+const {Deflate, Inflate} = require('pako/dist/pako.js');
 
 const API_URL = 'https://scsl.teamopen.dev/v0/';
 const TIMEOUT = 10000;
@@ -9,6 +10,38 @@ const hexOf = str => Buffer.from(str, 'utf8').toString('hex');
 
 const axiosReq = axios => (...args) => {
 	return axios.apply(null, args).then(res => res.data);
+};
+
+const niceChunk = 1024*1024; // 1MB
+const gzipNice = async (data, opts) => {
+  const deflate = new Deflate({
+    gzip: true,
+    ...(opts || {}),
+  });
+
+  console.log('Deflate Chunks =', Math.ceil(data.length / niceChunk));
+  for (let i = 0; i < data.length; i += niceChunk) {
+    await delay(1);
+    deflate.push(data.slice(i, i + niceChunk), i + niceChunk >= data.length);
+  }
+
+  if (deflate.err) throw deflate.err;
+  return deflate.result;
+};
+
+const ungzipNice = async (data, opts) => {
+  const inflate = new Inflate({
+    ...(opts || {}),
+  });
+
+  console.log('Inflate Chunks =', Math.ceil(data.length / niceChunk));
+  for (let i = 0; i < data.length; i += niceChunk) {
+    await delay(0);
+    inflate.push(data.slice(i, i + niceChunk), i + niceChunk >= data.length);
+  }
+
+  if (inflate.err) throw inflate.err;
+  return inflate.result;
 };
 
 exports.createRemoteClient = (axios, opts) => {
@@ -24,9 +57,9 @@ exports.createRemoteClient = (axios, opts) => {
       timeout: TIMEOUT,
     });
     const start = Date.now();
-    const pak = gzip(Buffer.from(JSON.stringify(res)));
+    const pak = await gzipNice(Buffer.from(JSON.stringify(res)));
     const mid = Date.now();
-    const out = JSON.parse(ungzip(pak, {to: 'string'}));
+    const out = JSON.parse(await ungzipNice(pak, {to: 'string'}));
     const took = [mid - start, Date.now() - mid];
     console.log('rezip', took);
     return out;
@@ -40,9 +73,9 @@ exports.createRemoteClient = (axios, opts) => {
       timeout: TIMEOUT,
     });
     const start = Date.now();
-    const pak = gzip(Buffer.from(JSON.stringify(res)));
+    const pak = await gzipNice(Buffer.from(JSON.stringify(res)));
     const mid = Date.now();
-    const out = JSON.parse(ungzip(pak, {to: 'string'}));
+    const out = JSON.parse(await ungzipNice(pak, {to: 'string'}));
     const took = [mid - start, Date.now() - mid];
     console.log('rezip', took);
     return out;
