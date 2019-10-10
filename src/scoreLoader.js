6c5e575b295a91c522c66d5ec22250f86538c360
Robin <robin@robin-it.com>
2019-10-10 18:39:26 +0200

    Space indent.

--- src/scoreLoader.js
+++ src/scoreLoader.js
@@ -14,49 +14,49 @@ const maxPerLoad = 10*oneMinute;
 
 exports.startLoadingScores = ({reloadSet, scoresDir, scDir, depMap, nodePath, cliPath, targetLoadTimeMins, meta, SOURCECRED_GITHUB_TOKEN}) => {
   const doneWhen = targetLoadTimeMins * oneMinute + Date.now();
-	let spawnQueue = Array.from(knuthShuffle([...reloadSet]));
-	let killTimeout;
-	let childToKill = null;
+  let spawnQueue = Array.from(knuthShuffle([...reloadSet]));
+  let killTimeout;
+  let childToKill = null;
 
-	const clearKillTimeout = () => {
-		if(killTimeout) {
-			clearTimeout(killTimeout);
-			killTimeout = null;
-		}
-	}
-	const setKillTimeout = () => {
-		clearKillTimeout();
-		killTimeout = setTimeout(() => {
-			childToKill.kill('SIGINT');
-			childToKill = null;
-		}, maxPerLoad);
-	};
+  const clearKillTimeout = () => {
+    if(killTimeout) {
+      clearTimeout(killTimeout);
+      killTimeout = null;
+    }
+  }
+  const setKillTimeout = () => {
+    clearKillTimeout();
+    killTimeout = setTimeout(() => {
+      childToKill.kill('SIGINT');
+      childToKill = null;
+    }, maxPerLoad);
+  };
 
-	process.on('SIGINT', () => {
-		clearKillTimeout();
-		if(childToKill) {
-			console.log('Received SIGINT. Forwarding this to our child process.');
-			spawnQueue = [];
-			childToKill.kill('SIGINT');
-		} else {
-			process.exit();
-		}
-	});
+  process.on('SIGINT', () => {
+    clearKillTimeout();
+    if(childToKill) {
+      console.log('Received SIGINT. Forwarding this to our child process.');
+      spawnQueue = [];
+      childToKill.kill('SIGINT');
+    } else {
+      process.exit();
+    }
+  });
 
-	const scoreNext = (ref) => {
+  const scoreNext = (ref) => {
     const jsonPath = pathJoin(scoresDir, `${hexOf(ref)}.json`);
     const jsonGzPath = `${jsonPath}.gz`;
-		const output = createWriteStream(jsonPath);
-		const scoreSourceCred = spawn(nodePath, [cliPath, 'scores', ref], {timeout: oneMinute, env: {SOURCECRED_DIRECTORY: scDir}});
-		childToKill = scoreSourceCred;
-		scoreSourceCred.stdout.pipe(output);
-		scoreSourceCred.stderr.pipe(process.stderr);
-		scoreSourceCred.on('close', async (code) => {
-			childToKill = null;
-			output.end();
-			meta.bumpScore(ref);
-			console.log(`child process exited with code ${code}`);
-			loadNext();
+    const output = createWriteStream(jsonPath);
+    const scoreSourceCred = spawn(nodePath, [cliPath, 'scores', ref], {timeout: oneMinute, env: {SOURCECRED_DIRECTORY: scDir}});
+    childToKill = scoreSourceCred;
+    scoreSourceCred.stdout.pipe(output);
+    scoreSourceCred.stderr.pipe(process.stderr);
+    scoreSourceCred.on('close', async (code) => {
+      childToKill = null;
+      output.end();
+      meta.bumpScore(ref);
+      console.log(`child process exited with code ${code}`);
+      loadNext();
 
       // Convert to gzip in parallel.
       await delay(0);
@@ -65,41 +65,41 @@ exports.startLoadingScores = ({reloadSet, scoresDir, scDir, depMap, nodePath, cl
       const jsonGz = gzip(json);
       await delay(0);
       writeFileSync(jsonGzPath, jsonGz);
-		});
-	};
+    });
+  };
 
-	let failedLoad = 0;
-	const loadNext = () => {
+  let failedLoad = 0;
+  const loadNext = () => {
     if(doneWhen <= Date.now()) {
       console.warn('Out of time, returning');
       return;
     }
 
-		const ref = spawnQueue.pop();
-		if(ref === undefined) {
-			if(failedLoad > 0) {
-				console.warn('One of the deps failed to load');
-			}
-			return;
-		}
+    const ref = spawnQueue.pop();
+    if(ref === undefined) {
+      if(failedLoad > 0) {
+        console.warn('One of the deps failed to load');
+      }
+      return;
+    }
 
-		const loadSourceCred = spawn(nodePath, [cliPath, 'load', ref], {timeout: maxPerLoad, env: {SOURCECRED_GITHUB_TOKEN, SOURCECRED_DIRECTORY: scDir}});
-		childToKill = loadSourceCred;
-		setKillTimeout();
-		loadSourceCred.stdout.pipe(process.stdout);
-		loadSourceCred.stderr.pipe(process.stderr);
-		loadSourceCred.on('close', (code) => {
-			clearKillTimeout();
-			childToKill = null;
-			console.log(`child process exited with code ${code}`);
-			failedLoad = Math.max(failedLoad, code);
-			if(code === 0) {
-				scoreNext(ref);
-			} else {
-				loadNext();
-			}
-		});
-	};
+    const loadSourceCred = spawn(nodePath, [cliPath, 'load', ref], {timeout: maxPerLoad, env: {SOURCECRED_GITHUB_TOKEN, SOURCECRED_DIRECTORY: scDir}});
+    childToKill = loadSourceCred;
+    setKillTimeout();
+    loadSourceCred.stdout.pipe(process.stdout);
+    loadSourceCred.stderr.pipe(process.stderr);
+    loadSourceCred.on('close', (code) => {
+      clearKillTimeout();
+      childToKill = null;
+      console.log(`child process exited with code ${code}`);
+      failedLoad = Math.max(failedLoad, code);
+      if(code === 0) {
+        scoreNext(ref);
+      } else {
+        loadNext();
+      }
+    });
+  };
 
-	loadNext();
+  loadNext();
 };
